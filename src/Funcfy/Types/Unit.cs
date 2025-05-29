3b7de183d6ac16f68cff19cc66b37000539b2dbf
leo-oliveira-eng <leo.oliveira.eng@outlook.com>
2025-05-29 00:24:21 -0300

    feat: add unit type

---
+++ src/Funcfy/Types/Unit.cs
@@ -0,0 +1,8 @@
+﻿namespace Funcfy.Types;
+
+/// <summary>
+/// Represents a type that has no value.
+/// In functional programming, this is often used to indicate the absence of a meaningful value, similar to `void` in other languages.
+/// This makes it particularly useful in functional contexts where a type is expected but no result is needed.
+/// </summary>
+public struct Unit { }
